7183226cfe6a61d82861ce117cfbee76e9b7d393
Simon Bruneaud <simon.bruneaud@ledger.fr>
2025-11-27 09:10:41 +0100

    test tree-shaking rolluo
    Signed-off-by: Simon Bruneaud <simon.bruneaud@ledger.fr>

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import './App.css'
-import { Button } from '@ledgerhq/ldls-ui-react'
+import { AmountInput, Button } from '@ledgerhq/ldls-ui-react'
+import { Android } from '@ledgerhq/ldls-ui-react/symbols';
+// import * as Octicons from '@primer/octicons-react'
+// import { Camera } from 'lucide-react';
+
 
 function App() {
   return (
     <div className='flex flex-col items-center justify-center w-full h-screen'>
-      <Button appearance='accent' size='sm'>Click me</Button>
-      
+      <Button icon={Android} asChild appearance='accent' size='sm'><a href='/'>Click me</a></Button>
+      <AmountInput onChange={() => {}} value={0} />
+      {/* <Octicons.AccessibilityIcon/>
+      <Octicons.BellFillIcon/> */}
+      {/* <Camera/> */}
       <div className='mt-12 bg-error-hover size-112'>test tokens</div>
     </div>
   )
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { visualizer } from 'rollup-plugin-visualizer'
 
 // https://vite.dev/config/
 export default defineConfig({
   css: {
     postcss: './postcss.config.js',
   },
-  plugins: [react()],
+  plugins: [
+    react(),
+    visualizer({
+      open: true,
+      filename: 'dist/stats.html',
+      gzipSize: true,
+      brotliSize: true,
+    }),
+  ],
 })
