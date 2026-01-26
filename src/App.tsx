b601a68189f4c1f7bc6a09d757b88e907cae2905
Simon Bruneaud <simon.bruneaud@ledger.fr>
2026-01-26 11:58:19 +0100

    mc
    Signed-off-by: Simon Bruneaud <simon.bruneaud@ledger.fr>

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,6 @@
 import { useState } from 'react';
 import './App.css';
-import { Button, ThemeProvider } from '@ledgerhq/lumen-ui-react';
-import TablePocGroupingTanstackExample from './components/TablePocGroupingTanstackExample';
-import TablePocSubGroupsAlternative from './components/TablePocSubGroupsAlternative';
-import TablePocSubGroups from './components/TablePocSubGroups';
-import { Sandbox } from './components/Sandbox';
+import { ThemeProvider } from '@ledgerhq/lumen-ui-react';
 
 type TableView = 'grouping-example' | 'subgroups-alternative' | 'subgroups' | 'sandbox';
 
@@ -12,40 +8,8 @@ function App() {
   const [activeTable, setActiveTable] = useState<TableView>('grouping-example');
 
   return (
-  <ThemeProvider defaultMode="light">
-    <div className='p-4'>
-      <div className='flex gap-2 mb-4'>
-        <Button
-          onClick={() => setActiveTable('grouping-example')}
-        >
-          Grouping Example
-        </Button>
-        
-        <Button
-          onClick={() => setActiveTable('subgroups-alternative')}
-        >
-          SubGroups Alternative
-        </Button>
-        
-        <Button
-          onClick={() => setActiveTable('subgroups')}
-        >
-          SubGroups
-        </Button>
-        <Button
-          onClick={() => setActiveTable('sandbox')}
-        >
-          App
-        </Button>
-      </div>
-
-      <div>
-        {activeTable === 'grouping-example' && <TablePocGroupingTanstackExample />}
-        {activeTable === 'subgroups-alternative' && <TablePocSubGroupsAlternative />}
-        {activeTable === 'subgroups' && <TablePocSubGroups />}
-        {activeTable === 'sandbox' && <Sandbox />}
-      </div>
-    </div>
+  <ThemeProvider colorScheme="light">
+    
     </ThemeProvider>
   );
 }
