d335ab162746c594865c5623e2a02f71e06ca250
Simon Bruneaud <simon.bruneaud@ledger.fr>
2026-03-11 10:22:43 +0100

    mc
    Signed-off-by: Simon Bruneaud <simon.bruneaud@ledger.fr>

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,8 @@
-import { useState } from 'react';
 import './App.css';
 import { ThemeProvider } from '@ledgerhq/lumen-ui-react';
 
-type TableView = 'grouping-example' | 'subgroups-alternative' | 'subgroups' | 'sandbox';
 
 function App() {
-  const [activeTable, setActiveTable] = useState<TableView>('grouping-example');
 
   return (
   <ThemeProvider colorScheme="light">
